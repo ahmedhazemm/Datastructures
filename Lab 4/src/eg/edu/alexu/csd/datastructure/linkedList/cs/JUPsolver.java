103f2b4683eeb3a31b49e26119f307f808603191
ahmedhazem <58703415+ahmedhazemm@users.noreply.github.com>
2020-03-21 02:13:45 +0200

    Lab4

---
+++ Lab 4/src/eg/edu/alexu/csd/datastructure/linkedList/cs/JUPsolver.java
@@ -0,0 +1,14 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUPsolver {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
